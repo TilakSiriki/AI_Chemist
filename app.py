9a5e364d96e852a3d30309356182d39face16449
TilakSiriki <127124595+TilakSiriki@users.noreply.github.com>
2024-12-25 21:14:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ with col2:
     uploaded_file = st.file_uploader("Upload an image of tablets (JPG, JPEG, PNG):", type=["jpg", "jpeg", "png"])
 
 if uploaded_file:
-    image = input_image_setup(uploaded_file) p
+    image = input_image_setup(uploaded_file) 
     st.image(image, caption="Uploaded Image", use_container_width=False)
 
 submit = st.button("🔍 Analyze Image")
